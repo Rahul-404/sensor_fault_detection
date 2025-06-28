450221dc866d93aede98cf7003abd7335a2fa2bb
Rahul-404 <rahulshelke981@gmail.com>
2025-06-28 14:21:04 +0530

    add constants for application configuration, database, environment variables, S3 bucket, and training pipeline

---
+++ sensor_fault_detection/constant/training_pipeline/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+TARGET_COLUMN = "class"
+PIPELINE_NAME: str = "sensor_fault"
+ARTIFACT_DIR: str = "artifact"
+
+FILE_NAME: str = "sensor_fault.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+PREPROCESSING_OBJECT_FILE_NAME: str = "preprocessing.pkl"
+MODEL_FILE_NAME = "model.pkl"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+"""Data Ingestion realted constants"""
+DATA_INGESTION_COLLECTION_NAME: str = "sensor_fault"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
