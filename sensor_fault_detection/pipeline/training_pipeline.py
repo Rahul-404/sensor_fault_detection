3d4984f5c31eeb82f326234a5bc42bc90167f2dd
Rahul-404 <rahulshelke981@gmail.com>
2025-06-28 23:13:29 +0530

    add data validation component with schema validation and drift detection

--- sensor_fault_detection/pipeline/training_pipeline.py
+++ sensor_fault_detection/pipeline/training_pipeline.py
@@ -1,9 +1,12 @@
 
 from sensor_fault_detection.components.data_ingestion import DataIngestion
+from sensor_fault_detection.components.data_validation import DataValidation
 
-from sensor_fault_detection.entity.config_entity import (DataIngestionConfig)
+from sensor_fault_detection.entity.config_entity import (DataIngestionConfig,
+                                                         DataValidationConfig)
 
-from sensor_fault_detection.entity.artifact_entity import (DataIngestionArtifact)
+from sensor_fault_detection.entity.artifact_entity import (DataIngestionArtifact,
+                                                           DataValidationArtifact)
 
 from sensor_fault_detection.exception import SensorFaultException
 from sensor_fault_detection.logger import logging
@@ -12,6 +15,7 @@ import sys
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifact:
         """
@@ -34,12 +38,40 @@ class TrainPipeline:
         except Exception as e:
             raise SensorFaultException(e, sys)
         
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting data validation component
+        """
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise SensorFaultException(e, sys)
 
     def run_pipeline(self) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
-            
-            logging.info(f"Training Pipeline is complete. {data_ingestion_artifact}")
+            data_validation_artifact = self.start_data_validation(
+                data_ingestion_artifact=data_ingestion_artifact
+            )
+            logging.info(f"Training Pipeline is complete. {data_validation_artifact}")
 
         except Exception as e:
             raise SensorFaultException(e, sys)
